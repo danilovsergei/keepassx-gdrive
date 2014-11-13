bd7057b4020bf1ad13c6063a0d11467c8ad2cfb7
Sergei Danilov <sergeidanilov@gmail.com>
2014-11-13 00:54:39 -0800

    removed not used google drive library files added google drive icon

---
+++ src/gdrive/GDriveConstants.h
@@ -0,0 +1,6 @@
+#ifndef GDRIVECONSTANTS_H
+#define GDRIVECONSTANTS_H
+#include <QtCore/QString>
+static const QString LAST_MODIFIED="LAST_MODIFIED";
+static const int LOGIN_WAIT_TIMEOUT = 2*60*1000;
+#endif // GDRIVECONSTANTS_H
